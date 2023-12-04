67196409dc100dd7f8070a5853f93316aa9294c5
Luana <luanacerqueira7766@gmail.com>
2023-12-04 15:57:34 -0300

    Ajeitando o index

---
+++ arrays.php
@@ -0,0 +1,221 @@
+<?php
+
+$produtos_promocao = [
+    [
+        "nome" => "Cortina Tonia Poliéster Branco 2,8x3m 1 Folha Inspire",
+        "foto" => "imagens/cortinas/1.jpg",
+        "preco_cortado" => "R$ 190,00",
+        "preco" => "R$ 150,00",
+        "descricao" => "Tamanho: 2,8x3m",
+        "categoria" => "cortinas",
+        "tela_inicial" => "promocao"
+    ],
+    [
+        "nome" => "Cortina Poliéster Bege 2,8x3m 1 Folha Inspire",
+        "foto" => "imagens/cortinas/2.jpg",
+        "preco_cortado" => "R$ 199,00",
+        "preco" => "R$ 152,90",
+        "descricao" => "Tamanho: 2,8x3m",
+        "categoria" => "cortinas",
+        "tela_inicial" => "promocao"
+    ],
+    [
+        "nome" => "Cortina Tonia Poliéster Branco e Marrom 2,8x3m",
+        "foto" => "imagens/cortinas/3.jpg",
+        "preco_cortado" => "R$ 160,00",
+        "preco" => "R$ 145,00",
+        "descricao" => "Tamanho: 2,8x3m",
+        "categoria" => "cortinas",
+        "tela_inicial" => "promocao"
+    ],
+    [
+        "nome" => "Cortina Tonia Poliéster Bege e Madeira 2,8x3m 1 Folha Inspire",
+        "foto" => "imagens/cortinas/4.jpeg",
+        "preco_cortado" => "R$ 189,00",
+        "preco" => "R$ 152,30",
+        "descricao" => "Tamanho: 2,8x3m",
+        "categoria" => "cortinas",
+        "tela_inicial" => "promocao"
+    ],
+    [
+        "nome" => "Persiana",
+        "foto" => "imagens/persianas/1.jpg",
+        "preco_cortado" => "R$ 222,00",
+        "preco" => "R$ 130,00",
+        "descricao" => "Tamanho: ",
+        "categoria" => "percianas",
+        "tela_inicial" => "promocao"
+    ],
+    [
+        "nome" => "Persiana Tonia - Preta - Folha Inspire",
+        "foto" => "imagens/persianas/2.jpg",
+        "preco_cortado" => "R$ 160,00",
+        "preco" => "R$ 130,00",
+        "descricao" => "Tamanho: ",
+        "categoria" => "percianas",
+        "tela_inicial" => "promocao"
+    ],
+    [
+        "nome" => "Persiana Tonia Bege - 2,8x3m 1 Folha Inspire",
+        "foto" => "imagens/divisorias hospitalares/1.jpg",
+        "preco_cortado" => "R$ 220,00",
+        "preco" => "R$ 184,00",
+        "descricao" => "Tamanho: ",
+        "categoria" => "percianas",
+        "tela_inicial" => "promocao"
+    ],
+    [
+        "nome" => "Cortina Tonia Poliéster Branco 2,8x3m 1 Folha Inspire",
+        "foto" => "imagens/cortinas/5.jpeg",
+        "preco_cortado" => "R$ 220,00",
+        "preco" => "R$ 152,00",
+        "descricao" => "Tamanho: ",
+        "categoria" => "cortinas",
+        "tela_inicial" => "promocao"
+    ],
+    [
+        "nome" => "Cortina Pharrel verde e branca com detalhes 1,40x2,80m 1 Folha Inspire",
+        "foto" => "imagens/cortinas/6.jpg",
+        "preco_cortado" => "R$ ",
+        "preco" => "R$ 250,00",
+        "descricao" => "Tamanho: 1,40x2,80m",
+        "categoria" => "cortinas",
+        "tela_inicial" => "mais vendidas"
+    ],
+    [
+        "nome" => "Cortina Pharrel branca com detalhes 1,40x2,80m 1 Folha Inspire",
+        "foto" => "imagens/cortinas/8.webp",
+        "preco_cortado" => "R$ ",
+        "preco" => "R$ 130,00",
+        "descricao" => "Tamanho: 1,40x2,80m",
+        "categoria" => "cortinas",
+        "tela_inicial" => "mais vendidas"
+    ],
+    [
+        "nome" => "Persiana Pharrel preta com detalhes 1,40x2,80m 1 Folha Inspire",
+        "foto" => "imagens/persianas/6.jpg",
+        "preco_cortado" => "R$ ",
+        "preco" => "R$ 140,00",
+        "descricao" => "Tamanho: 1,40x2,80m",
+        "categoria" => "percianas",
+        "tela_inicial" => "mais vendidas"
+    ],
+    [
+        "nome" => "Cortina Pharrel bege com detalhes 1,40x2,80m 1 Folha Inspire",
+        "foto" => "imagens/cortinas/2.jpg",
+        "preco_cortado" => "R$ ",
+        "preco" => "R$ 145,00",
+        "descricao" => "Tamanho: 1,40x2,80m",
+        "categoria" => "cortinas",
+        "tela_inicial" => "mais vendidas"
+    ],
+    [
+        "nome" => "Persiana Pharrel branca com detalhes 1,40x2,80m 1 Folha Inspire",
+        "foto" => "imagens/persianas/7.jpg",
+        "preco_cortado" => "R$ ",
+        "preco" => "R$ 130,00",
+        "descricao" => "Tamanho: 1,40x2,80m",
+        "categoria" => "percianas",
+        "tela_inicial" => "mais vendidas"
+    ],
+    [
+        "nome" => "Persiana Pharrel preta com detalhes 1,40x2,80m 1 Folha Inspire",
+        "foto" => "imagens/persianas/1.jpg",
+        "preco_cortado" => "R$ ",
+        "preco" => "R$ 135,00",
+        "descricao" => "Tamanho: 1,40x2,80m",
+        "categoria" => "percianas",
+        "tela_inicial" => "mais vendidas"
+    ],
+    [
+        "nome" => "Cortina Pharrel marrom e branca com detalhes 1,40x2,80m 1 Folha Inspire",
+        "foto" => "imagens/cortinas/3.jpg",
+        "preco_cortado" => "R$ ",
+        "preco" => "R$ 145,00",
+        "descricao" => "Tamanho: 1,40x2,80m",
+        "categoria" => "cortinas",
+        "tela_inicial" => "mais vendidas"
+    ],
+    [
+        "nome" => "Cortina Pharrel Branca com detalhes 1,40x2,80m 1 Folha Inspire",
+        "foto" => "imagens/cortinas/1.jpg",
+        "preco_cortado" => "R$ ",
+        "preco" => "R$ 138,00",
+        "descricao" => "Tamanho: 1,40x2,80m",
+        "categoria" => "cortinas",
+        "tela_inicial" => "mais vendidas"
+    ], 
+
+    [
+        "nome" => "Cortina Pharrel Bege e Marrom com detalhes ao redor 1,40x2,80m 1 Folha Inspire",
+        "foto" => "imagens/novidades/1.jpg",
+        "preco_cortado" => "R$ ",
+        "preco" => "R$ 170,00",
+        "descricao" => "Tamanho: 1,40x2,80m",
+        "categoria" => "cortinas",
+        "tela_inicial" => "novidades"
+    ],
+    [
+        "nome" => "Cortina Pharrel Bege 1,40x2,80m 1 Folha Inspire",
+        "foto" => "imagens/novidades/2.jpg",
+        "preco_cortado" => "R$ ",
+        "preco" => "R$ 134,00",
+        "descricao" => "Tamanho: ",
+        "categoria" => "cortinas",
+        "tela_inicial" => "novidades"
+    ],
+    [
+        "nome" => "Cortina Pharrel Bege e madeira 1,40x2,80m 1 Folha Inspire",
+        "foto" => "imagens/novidades/3.jpeg",
+        "preco_cortado" => "R$ ",
+        "preco" => "R$ 156,00",
+        "descricao" => "Tamanho: 1,40x2,80m",
+        "categoria" => "cortinas",
+        "tela_inicial" => "novidades"
+    ],
+    [
+        "nome" => "Cortina Pharrel Bege e branco 1,40x2,80m 1 Folha Inspire",
+        "foto" => "imagens/novidades/4.webp",
+        "preco_cortado" => "R$ ",
+        "preco" => "R$ 215,00",
+        "descricao" => "Tamanho: 1,40x2,80m",
+        "categoria" => "cortinas",
+        "tela_inicial" => "novidades"
+    ],
+    [
+        "nome" => "Cortina Pharrel branca, Bege e Marrom 1,40x2,80m 1 Folha Inspire",
+        "foto" => "imagens/novidades/5.jpeg",
+        "preco_cortado" => "R$ ",
+        "preco" => "R$ 174,00",
+        "descricao" => "Tamanho: 1,40x2,80m",
+        "categoria" => "cortinas",
+        "tela_inicial" => "novidades"
+    ],
+    [
+        "nome" => "Cortina Pharrel Bege e Marrom, com tecido detalhado 1,40x2,80m 1 Folha Inspire",
+        "foto" => "imagens/novidades/6.jpg",
+        "preco_cortado" => "R$ ",
+        "preco" => "R$ 138,00",
+        "descricao" => "Tamanho: 1,40x2,80m",
+        "categoria" => "cortinas",
+        "tela_inicial" => "novidades"
+    ],
+    [
+        "nome" => "Cortina Pharrel Bege e Marrom com detalhes floridos embutidos 1,40x2,80m 1 Folha Inspire",
+        "foto" => "imagens/novidades/7.jpg",
+        "preco_cortado" => "R$ ",
+        "preco" => "R$ 185,00",
+        "descricao" => "Tamanho: 1,40x2,80m",
+        "categoria" => "cortinas",
+        "tela_inicial" => "novidades"
+    ],
+    [
+        "nome" => "Cortina Pharrel Branca 1,40x2,80m 1 Folha Inspire",
+        "foto" => "imagens/novidades/8.webp",
+        "preco_cortado" => "R$ ",
+        "preco" => "R$ 110,00",
+        "descricao" => "Tamanho: 1,40x2,80m",
+        "categoria" => "cortinas",
+        "tela_inicial" => "novidades"
+    ]  
+];
---
+++ index.php
@@ -0,0 +1,182 @@
+<?php
+require "arrays.php";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="estilo.css" rel="stylesheet">
+    <title>Dona Maria Cortinas & Cia</title>
+</head>
+
+<body>
+    <header>
+        <div class="logo-topo">
+            <a href="index.html"><img src="imagens/elementos-do-site/logo-dona-maria-1.png"></a>
+        </div>
+        
+        <div class="barra-pesquisa">
+            <form action="" method="post">
+                <label>
+                    <input type="text" id="nome" name="nome" placeholder="O que você procura?" required>
+                </label>
+
+                <button><img src="imagens/elementos-do-site/lupa.png"></button>
+            </form>
+        </div>
+
+        <div id="menu-topo">
+            <ul>
+                <li><a href="paginas/login.html"><img src="imagens/elementos-do-site/entrar.png"></a></li>
+                <li><a href="paginas/favoritos.html"><img src="imagens/elementos-do-site/favoritos.png"></a></li>
+                <li><a href="paginas/carrinho.html"><img src="imagens/elementos-do-site/carrinho.png"></a></li>
+            </ul>
+        </div>
+
+    </header>
+
+    <main><!--Corpo do Site-->
+
+        <div class="banner-top"><!--Banner-->
+            <a href="">
+                <img src="imagens/banner/cortinas-banner.jpg" alt="banner">
+            </a>
+        </div>
+
+        <section class="categorias"><!--Categorias-->
+            <div class="cat">
+                <h1>Cortinas</h1>
+                <div><a><img src="imagens/cortinas/1.jpg" alt=""></a></div>
+            </div>
+            <div class="cat">
+                <h1>Percianas</h1>
+                <div><a><img src="imagens/persianas/1.jpg" alt=""></a></div>
+            </div>
+            <div class="cat">
+                <h1>Divisoria hospitalares</h1>
+                <div><a><img src="imagens/divisorias hospitalares/1.jpg" alt=""></a></div>
+            </div>
+            <div class="cat">
+                <h1>Toldos</h1>
+                <div><a><img src="imagens/toldos/1.jpg" alt=""></a></div>
+            </div>
+        </section>
+
+        <section class="categoria">
+
+            <div class="banner-categoria"><!--Banner-->
+                <a href="">
+                    <img src="imagens/banner/banner-promocao.png" alt="banner da categoria">
+                </a>
+            </div>
+
+            <div class="titulo-categoria"><h1>Aqui estão as Peças em Promoção</h1></div>
+            
+            
+                <div class="itens">
+                    <?php
+                        foreach($produtos_promocao as $i => $f){
+                            if($f["tela_inicial"] == "promocao"){ 
+                    ?>
+                    <div class="itens2">
+                        <a href=""></a>
+                        <div class="itens-img">
+                            <img src="<?=$f["foto"]?>" alt="">
+                        </div>
+                        <div class="itens-descricao">
+                            <div><p class="texto-limitado"><?=$f["nome"]?></p></div>
+                            <p><s><?=$f["preco_cortado"]?></s></p>
+                            <h1><?=$f["preco"]?></h1>
+                            <button>Comprar</button>
+                        </div>
+                    <div>
+                    <?php
+                        }}
+                    ?>
+                </div>
+            
+                
+        </section>
+
+        <section class="categoria">
+
+            <div class="banner-categoria"><!--Banner-->
+                <a href="">
+                    <img src="imagens/banner/banner-mais-vendidas.png" alt="banner da categoria">
+                </a>
+            </div>
+
+            <div class="titulo-categoria"><h1>Aqui estão as Peças mais Vendidas</h1></div>
+            
+            <div class="bloco-itens">
+            
+            <div class="itens">
+                    <?php
+                        foreach($produtos_promocao as $i => $f){
+                            if($f["tela_inicial"] == "mais vendidas"){ 
+                    ?>
+                    <div class="itens2">
+                        <a href=""></a>
+                        <div class="itens-img">
+                            <img src="<?=$f["foto"]?>" alt="">
+                        </div>
+                        <div class="itens-descricao">
+                            <div><p class="texto-limitado"><?=$f["nome"]?></p></div>
+                            <h1><?=$f["preco"]?></h1>
+                            <button>Comprar</button>
+                        </div>
+                    <div>
+                    <?php
+                        }}
+                    ?>
+                </div>
+
+            </div>
+                
+        </section>
+
+        <section class="categoria">
+
+            <div class="banner-categoria"><!--Banner-->
+                <a href="">
+                    <img src="imagens/banner/banner-novidade.png" alt="banner da categoria">
+                </a>
+            </div>
+
+            <div class="titulo-categoria"><h1>Aqui estão as Novidades</h1></div>
+            
+            <div class="bloco-itens">
+            <div class="itens">
+                    <?php
+                        foreach($produtos_promocao as $i => $f){
+                            if($f["tela_inicial"] == "novidades"){ 
+                    ?>
+                    <div class="itens2">
+                        <a href=""></a>
+                        <div class="itens-img">
+                            <img src="<?=$f["foto"]?>" alt="">
+                        </div>
+                        <div class="itens-descricao">
+                            <div><p class="texto-limitado"><?=$f["nome"]?></p></div>
+                            <h1><?=$f["preco"]?></h1>
+                            <button>Comprar</button>
+                        </div>
+                    <div>
+                    <?php
+                        }}
+                    ?>
+                </div>
+            </div>
+                
+        </section>
+        
+    </main>
+
+    <footer><!--Rodapé do Site-->
+            <p>© 2023 Dona Maria Cortinas e Cia. Todos os direitos reservados</p>    
+    </footer>
+</body>
+
+</html>
